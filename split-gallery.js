11d93e0013083c9110cf34af056a37afa241a8bc
dani-mello <info@aorakisolutions.co.nz>
2026-01-05 15:05:23 +1300

    Update split-gallery.js

--- split-gallery.js
+++ split-gallery.js
@@ -28,7 +28,7 @@ gsap.registerPlugin(ScrollTrigger);
 
     // ===== Desktop settings (LOCKED) =====
     const DESKTOP = {
-      cardWRem: 100%,
+      cardWRem: 50,
       cardHRem: 50,
       minScale: 0.5,
       falloff: 0.55,
