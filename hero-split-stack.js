1fe8896158afe123cdc2594e21c967941523f3b6
dani-mello <info@aorakisolutions.co.nz>
2026-01-10 08:26:01 +1300

    Update hero-split-stack.js

--- hero-split-stack.js
+++ hero-split-stack.js
@@ -1,5 +1,5 @@
 console.log(
-  "%cSPLIT STACK JS LOADED (V15 - FADE IN + TRUE OFFSCREEN BURST)",
+  "%cSPLIT STACK JS LOADED (V16 - FADE IN + TRUE OFFSCREEN BURST)",
   "background:#0a1925;color:#fcb124;padding:4px 8px;border-radius:4px;font-weight:bold;"
 );
 
